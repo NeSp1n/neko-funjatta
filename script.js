0d7e25daee7e0cf1b0d7d553a6572d4c40b55eef
NeSp1n <dcdavid109@gmail.com>
2023-04-02 14:34:21 -0600

    agregada base

---
+++ script.js
@@ -0,0 +1,14 @@
+var fecha = new Date();
+//var hola = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+
+
+
+
+function nekoFunjatta(){
+
+    fechaActual = `${fecha.getDate()}/${fecha.getMonth()}/${fecha.getFullYear()}`
+
+    //alert(fechaActual);
+    document.getElementById('fecha').innerHTML = fechaActual
+    
+}
